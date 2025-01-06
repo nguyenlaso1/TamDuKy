9e6900243c13ab1f2fefa5817f6c0090ef643372
nguyenlaso1 <ngoquangnguyen2504@gmail.com>
2025-01-06 22:25:26 +0700

    upload

---
+++ library.js
@@ -0,0 +1,38 @@
+const arrowBtns = document.querySelectorAll(".thuvien .arrow");
+const carousel = document.querySelector(".thuvien .carousel");
+const cards = document.querySelectorAll(".thuvien .card");
+const firstCardWidth = document.querySelector(".thuvien .card").offsetWidth;
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "left" ? - 230 : 230;
+    })
+})
+
+function momodal1(){
+    var query = document.querySelector("#tt-sk-cd .tintuc-sukien .description1");
+    if (query.classList.contains("active"))
+        query.classList.remove("active");
+    else query.classList.add("active");
+}
+
+function momodal2(){
+    var query = document.querySelector("#tt-sk-cd .tintuc-sukien .description2");
+    if (query.classList.contains("active"))
+        query.classList.remove("active");
+    else query.classList.add("active");
+}
+
+function momodal3(){
+    var query = document.querySelector("#tt-sk-cd .tintuc-sukien .description3");
+    if (query.classList.contains("active"))
+        query.classList.remove("active");
+    else query.classList.add("active");
+}
+
+function momodal4(){
+    var query = document.querySelector("#tt-sk-cd .tintuc-sukien .description4");
+    if (query.classList.contains("active"))
+        query.classList.remove("active");
+    else query.classList.add("active");
+}
